b8197c36c0216f8be7637403581fae566ccdaa33
Lucas Maracaipe <maracaipelucas@gmail.com>
2021-06-16 19:01:28 -0300

    Novos parâmetros de condições

--- Cavaleiro/knight.c
+++ Cavaleiro/knight.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+static int novoPontoI = 0;
+static int novoPontoJ = 0;
 static int matriz[8][8];
 static int maiorValor = 1;
 static int pontoInicial = 1;
 static bool matrizIncompleta = true;
-static int pontosCondenados[100][4];
+static int pontosCondenados[500][5];
 static int possiveisJogadas[8][2] = {
     {2, 1}, //k = 0
     {2, -1},
@@ -26,7 +28,6 @@ void procurarUltimoPonto()
             if (matriz[i][j] > maiorValor)
             {
                 maiorValor = matriz[i][j];
-                procurarUltimoPonto();
             }
         }
     }
@@ -66,11 +67,11 @@ void imprimirMatriz()
     printf("\n------------------\n");
 }
 
-void imprimirMatrizCondenada(int matriz[100][4])
+void imprimirMatrizCondenada(int matriz[500][5])
 {
-    for (int i = 0; i <= 99; i++)
+    for (int i = 0; i <= 499; i++)
     {
-        for (int j = 0; j <= 3; j++)
+        for (int j = 0; j <= 4; j++)
         {
             printf("%i ", matriz[i][j]);
         }
@@ -92,62 +93,67 @@ void preencherMatriz()
     matriz[0][4] = pontoInicial;
 }
 
-void preencherMatrizCondenada(int matriz[100][4])
+void preencherMatrizCondenada(int matriz[500][5])
 {
-    for (int i = 0; i <= 99; i++)
+    for (int i = 0; i <= 499; i++)
     {
-        for (int j = 0; j <= 3; j++)
+        for (int j = 0; j <= 4; j++)
         {
             matriz[i][j] = 0;
         }
     }
 }
 
-void condenarProximaCasa(int xAtual, int yAtual, int xProx, int yProx)
+void condenarProximaCasa(int xAtual, int yAtual, int xProx, int yProx, int numeroAtual)
 {
     int j = 0;
     bool coordNaoExiste = true;
-    for (int i = 0; i <= 99; i++)
+    for (int i = 0; i <= 499; i++)
     {
         if (pontosCondenados[i][j] == xAtual &&
             pontosCondenados[i][j + 1] == yAtual &&
             pontosCondenados[i][j + 2] == xProx &&
-            pontosCondenados[i][j + 3] == yProx)
+            pontosCondenados[i][j + 3] == yProx &&
+            pontosCondenados[i][j + 4] == numeroAtual)
         {
+            printf("Jogada já foi condenada anteriormente!\n");
             coordNaoExiste = false;
-            i = 99;
+            i = 499;
         }
     }
 
     if (coordNaoExiste)
     {
-        for (int i = 0; i <= 99; i++)
+        for (int i = 0; i <= 499; i++)
         {
             if (pontosCondenados[i][j] == 0 &&
                  pontosCondenados[i][j + 1] == 0 &&
                  pontosCondenados[i][j + 2] == 0 &&
-                 pontosCondenados[i][j + 3] == 0) 
+                 pontosCondenados[i][j + 3] == 0 &&
+                 pontosCondenados[i][j + 4] == 0) 
             {
-                printf("Condenando jogada... (%i, %i) -> (%i,%i)\n", xAtual, yAtual, xProx, yProx);
+                printf("Condenando jogada... (%i)[%i, %i] -> [%i,%i]\n", numeroAtual, xAtual, yAtual, xProx, yProx);
                 pontosCondenados[i][j] = xAtual;
                 pontosCondenados[i][j + 1] = yAtual;
                 pontosCondenados[i][j + 2] = xProx;
                 pontosCondenados[i][j + 3] = yProx;
-                i = 99;
+                pontosCondenados[i][j + 4] = numeroAtual;
+                i = 499;
             }
         }
     }
 }
 
-bool proximaCasaNaoEstaCondenada(int xAtual, int yAtual, int xProx, int yProx)
+bool proximaCasaNaoEstaCondenada(int xAtual, int yAtual, int xProx, int yProx, int numeroAtual)
 {
     int j = 0;
-    for (int i = 0; i <= 99; i++)
+    for (int i = 0; i <= 499; i++)
     {
         if (pontosCondenados[i][j] == xAtual &&
             pontosCondenados[i][j + 1] == yAtual &&
             pontosCondenados[i][j + 2] == xProx &&
-            pontosCondenados[i][j + 3] == yProx)
+            pontosCondenados[i][j + 3] == yProx &&
+            pontosCondenados[i][j + 4] == numeroAtual)
         {
             printf("Próxima casa ESTÁ condenada\n\n");
             return false;
@@ -158,18 +164,19 @@ bool proximaCasaNaoEstaCondenada(int xAtual, int yAtual, int xProx, int yProx)
     return true;
 }
 
-void retrocederECondenar(int xAtual, int yAtual, int xProx, int yProx)
+void retrocederECondenar(int xAtual, int yAtual, int xProx, int yProx, int numeroAtual)
 {
     matriz[xAtual][yAtual] = 0;
+    printf("Casa Atual ZERADA\n");
+    imprimirMatriz();
 
     if (maiorValor >= 1)
     {
         maiorValor = maiorValor - 1;
         printf("Retornando para o número: %i\n", maiorValor);
     }
-
-    condenarProximaCasa(xAtual, yAtual, xProx, yProx);
-    procurarUltimoPonto();
+    condenarProximaCasa(xAtual, yAtual, xProx, yProx, numeroAtual);
+    //procurarUltimoPonto();
 }
 
 bool menorQueLimite(int xFuturo, int yFuturo)
@@ -191,7 +198,7 @@ bool movimentoFuturoDisponivel(int futuraJogadaX, int futuraJogadaY)
         return true;
     }
 
-    printf("Próxima casa está indisponível:\nNúmero: %i - [X futuro:%i, Y futuro:%i]\n\n",matriz[futuraJogadaX][futuraJogadaY], futuraJogadaX, futuraJogadaY);
+    printf("Próxima casa está indisponível:\nOcupante: %i - [%i,%i]\n\n",matriz[futuraJogadaX][futuraJogadaY], futuraJogadaX, futuraJogadaY);
     return false;
 }
 
@@ -201,6 +208,7 @@ void preencherProximaCasa()
     {
         for (int j = 0; j <= 7; j++)
         {
+            EncontrarCasaAtual:
             if (matriz[i][j] == maiorValor)
             {
                 for (int k = 0; k <= 7; k++)
@@ -212,19 +220,26 @@ void preencherProximaCasa()
                         int futuraJogadaX = i + possiveisJogadas[k][l];
                         int futuraJogadaY = j + possiveisJogadas[k][l + 1];
 
+                        if(maiorValor == 60)
+                        {
+                            imprimirMatrizCondenada(pontosCondenados);
+                            exit(0);
+                        }
+
                         printf("Futura jogada: Número: %i - X:%i, Y: %i\nMovimento do cavalo: [%i,%i]\n",maiorValor+1, futuraJogadaX, futuraJogadaY, possiveisJogadas[k][l], possiveisJogadas[k][l + 1]);
 
-                        if (menorQueLimite(futuraJogadaX, futuraJogadaY) && movimentoFuturoDisponivel(futuraJogadaX, futuraJogadaY) && proximaCasaNaoEstaCondenada(i, j, futuraJogadaX, futuraJogadaY))
+                        if (menorQueLimite(futuraJogadaX, futuraJogadaY) && movimentoFuturoDisponivel(futuraJogadaX, futuraJogadaY) && proximaCasaNaoEstaCondenada(i, j, futuraJogadaX, futuraJogadaY, maiorValor))
                         {
                             matriz[futuraJogadaX][futuraJogadaY] = maiorValor + 1;
                             maiorValor = maiorValor + 1;
                             imprimirMatriz();
                             preencherProximaCasa();
                         }
-                        else if( movimentoFuturoDisponivel(futuraJogadaX, futuraJogadaY) && proximaCasaNaoEstaCondenada(i, j, futuraJogadaX, futuraJogadaY))
+                        else if(!movimentoFuturoDisponivel(futuraJogadaX, futuraJogadaY) && proximaCasaNaoEstaCondenada(i, j, futuraJogadaX, futuraJogadaY, maiorValor))
                         {
                             printf("\n---RETROCEDENDO---\n");
-                            retrocederECondenar(i, j, futuraJogadaX, futuraJogadaY);
+                            retrocederECondenar(i, j, futuraJogadaX, futuraJogadaY, maiorValor);
+
                             for (int m = 0; m <= 7; m++)
                             {
                                 for (int n = 0; n <= 7; n++)
@@ -233,6 +248,8 @@ void preencherProximaCasa()
                                     {
                                         i = m;
                                         j = n;
+                                        printf("----Novo ponto atual: (%i)[%i,%i]\n\n", maiorValor, i, j);
+                                        goto EncontrarCasaAtual;
                                     }
                                 }
                             }
